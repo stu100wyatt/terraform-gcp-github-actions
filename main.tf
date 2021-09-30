a80269807b93bd7a604726c9205ae8f3c667b1cc
stu100wyatt <swyatt@paloaltonetworks>
2021-09-30 10:57:39 +0100

    new configuration

--- main.tf
+++ main.tf
@@ -1,12 +1,11 @@
 resource "google_storage_bucket" "bucket_sw_1" {
-  name = "sw-bucket-random-iac"
+  name = "sw-bucket-random-iac-v2"
   labels = {
-    yor_trace = "10a0b1c9-5594-4fcb-b1c0-fa0ff48c8656"
   }
 }
 
 resource "google_storage_bucket" "gcs_bucket" {
-  name = "sw-bucket-random-001125"
+  name = "sw-bucket-random-001125-v3"
   labels = {
   }
 }
