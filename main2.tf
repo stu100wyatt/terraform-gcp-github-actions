0329dc532af64059c71cdef509c88e7df7825995
stu100wyatt <swyatt@paloaltonetworks>
2022-05-13 10:21:35 +0100

    new file

---
+++ main2.tf
@@ -0,0 +1,24 @@
+
+
+resource "google_storage_bucket" "gcs-new-bucket-v1-22" {
+  name     = "gcs-new-bucket-v1.5-22"
+  location = "EU"
+  labels = {
+  }
+  uniform_bucket_level_access = true
+}
+
+resource "google_storage_bucket" "gcs-new-bucket-v2" {
+  name     = "gcs-new-bucket-v2-22"
+  location = "EU"
+  labels = {
+}
+}
+resource "google_storage_bucket" "gcs-new-bucket-v3" {
+  name     = "gcs-new-bucket-v3-22"
+  location = "EU"
+  labels = {
+
+  }
+  uniform_bucket_level_access = true
+}
